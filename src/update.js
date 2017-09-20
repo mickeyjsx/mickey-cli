9bd184579c8567513a70b508f74edfd671464bbb
pengxingjian.pxj <pengxingjian.pxj@alibaba-inc.com>
2017-09-20 11:01:52 +0800

    update checker

---
+++ src/update.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-console */
+import chalk from 'chalk'
+import updateNotifier from 'update-notifier'
+import pkg from '../package.json'
+
+const notifier = updateNotifier({ pkg })
+const update = notifier.update || {
+  latest: '1.0.1',
+  current: '1.0.0',
+}
+
+if (update) {
+  const latest = update.latest
+  const current = update.current
+
+  console.log()
+  console.log(chalk.gray('     ----------------------------------------------'))
+  console.log(`    ${chalk.gray('/')}        update available ${chalk.gray(current)} → ${chalk.green(latest)}        ${chalk.gray('\\')}`)
+  console.log(`    ${chalk.gray('\\')}    run ${chalk.magenta('(sudo) npm i -g logo.svg')} to update    ${chalk.gray('/')}`)
+  console.log(chalk.gray('     ----------------------------------------------'))
+}
