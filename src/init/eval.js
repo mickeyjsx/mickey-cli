793bbeb5d6e861e887956022acb21b97d8185fe6
pengxingjian.pxj <pengxingjian.pxj@alibaba-inc.com>
2018-05-02 20:21:24 +0800

    enhance eval

--- src/init/eval.js
+++ src/init/eval.js
@@ -1,7 +1,12 @@
 import chalk from 'chalk'
+import { isFunction } from '../utils'
 
 // Evaluate an expression in metadata.json in the context of prompt answers data.
 export default function evaluate(exp, data) {
+  if (isFunction(exp)) {
+    return exp(data)
+  }
+
   const fn = new Function('data', `with (data) { return ${exp}}`) // eslint-disable-line
   try {
     return fn(data)
